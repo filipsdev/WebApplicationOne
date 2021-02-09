ba0bccae7ec85731ddaef3d519f33532aa97a296
FS Coding <sotirovf@gmail.com>
2021-02-09 18:37:30 +0000

    DB created

--- data/DSCdata.sql
+++ data/DSCdata.sql
@@ -1,10 +1,12 @@
-CREATE TABLE Items(
-   Name        VARCHAR(30) NOT NULL PRIMARY KEY
-  ,Type        VARCHAR(30)
-  ,Size        INTEGER  NOT NULL
-  ,Price       VARCHAR(30) NOT NULL
-  ,Description VARCHAR(200)
+﻿CREATE TABLE [dbo].[Items](
+   [ID]			INT	IDENTITY (1,1) NOT NULL
+  ,[Name]        VARCHAR(30) NOT NULL
+  ,[Type]        VARCHAR(30) NULL
+  ,[Size]        INTEGER  NULL
+  ,[Price]       DECIMAL(10,2) NULL
+  ,[Description] VARCHAR(200) NULL
+  ,PRIMARY KEY CLUSTERED ([ID] ASC)
 );
-INSERT INTO mytable(Name,Type,Size,Price,Description) VALUES ('Levi�s Jeans','Men',40,'�25.00','The best jeans.');
-INSERT INTO mytable(Name,Type,Size,Price,Description) VALUES ('Dress','Woman',25,'�34.99','Amazing dress.');
-INSERT INTO mytable(Name,Type,Size,Price,Description) VALUES ('Tie',NULL,12,'�5.00',NULL);
+INSERT INTO dbo.Items(Name,Type,Size,Price,Description) VALUES ('Levi�s Jeans','Men',40,'25.00','The best jeans.');
+INSERT INTO dbo.Items(Name,Type,Size,Price,Description) VALUES ('Dress','Woman',25,'34.99','Amazing dress.');
+INSERT INTO dbo.Items(Name,Type,Size,Price,Description) VALUES ('Tie',NULL,12,'5.00',NULL);
