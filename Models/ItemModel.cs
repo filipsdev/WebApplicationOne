e5e9880fbe0f97a445b18dde925239be3da6471f
FS Coding <sotirovf@gmail.com>
2021-02-21 18:08:48 +0000

    changing the DisplayName of model properties

--- Models/ItemModel.cs
+++ Models/ItemModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,7 @@ namespace WebApplicationOne.Models
         public int Id { get; set; }
 
         [Required]
+        [DisplayName ("Item Name")]
         public string Name { get; set; }
         [Required]
         public string Type { get; set; }
